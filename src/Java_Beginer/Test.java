0ba1a7e0e35171a262d235f6762496d1ac1f6366
lepriccon <BezAlex95@gmail.com>
2022-08-02 13:52:27 +0300

    something

--- src/Java_Beginer/Test.java
+++ src/Java_Beginer/Test.java
@@ -1,16 +1,52 @@
 package Java_Beginer;
 
 import java.nio.charset.StandardCharsets;
+import java.util.Comparator;
+import java.util.Set;
+import java.util.TreeSet;
 
 public class Test {
     public static void main(String[] args) {
-        String alphabet = "abcdefghijklmnopqrstuvwxyz";
-        char[] b = alphabet.toCharArray();
-        for (char a :
-                b) {
-            System.out.println(a + "-->" + (int)a);
-
+        Comparator<testObject> comp = new myComparatorIntS().thenComparing(new myComparator());
+        Set<testObject> mySet = new TreeSet<>(comp);
+        mySet.add(new testObject("A", 2));
+        mySet.add(new testObject("C", 0));
+        mySet.add(new testObject("A", 5));
+        mySet.add(new testObject("B", 7));
+        mySet.add(new testObject("D", 5));
+        for (testObject newT : mySet){
+            System.out.println(newT.text + " ---- " + newT.index);
         }
+        }
+    }
+    class testObject{
+        String text;
+        int index;
+
+        public testObject(String text, int index) {
+            this.text = text;
+            this.index = index;
+        }
+
+         String getName (){
+         return  this.text;
+        }
+
+    }
+
+    class myComparator implements  Comparator<testObject>{
+        @Override
+        public int compare(testObject o1, testObject o2) {
+            return o1.text.compareTo(o2.text);
+        }
+
+    }
+
+class myComparatorIntS implements  Comparator<testObject>{
+    @Override
+    public int compare(testObject o1, testObject o2) {
+        return o1.index - o2.index;
     }
 
 }
+
